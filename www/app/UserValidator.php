a3f82f6d46bd7a61f3980ecec387e49a3e4d237d
João Harbs <harbsprog@gmail.com>
2020-01-04 21:34:05 -0300

    fix ambient, create laravel project and init auth implementation

---
+++ www/app/UserValidator.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+class UsersValidator
+{
+    public const ERROR_MESSAGES = [
+        'required'       => 'O campo :attribute é obrigatório',
+        'numeric'        => 'O valor do campo deve ser numérico',
+        'max'            => 'O :attribute deve ter no máximo :max caracteres',
+        'min'            => 'O :attribute deve ter no mínimo :min caracteres',
+        'email'          => 'O campo :attribute deve conter um e-mail válido'
+    ];
+    public const NEW_PACKAGE_RULE = [
+        'name'          => 'required|string',
+        'email'          => 'required|email|unique:users',
+        'password'       => 'required|string|min:6|max:10'
+    ];
+}
---
+++ www/routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('auth')->group(
+    function () {
+        Route::post('login', 'AuthController@authenticate');
+        Route::post('register', 'AuthController@register');
+    }
+);
+
+Route::group(
+    ['middleware' => 'auth.jwt'],
+    function () {
+    }
+);
